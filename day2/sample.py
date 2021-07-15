2a4f88215d215a4924e43c0968982b07017c7287
Hari Gopal <gopalhari55@gmail.com>
2021-07-15 10:29:12 +0530

    2nd day practice

---
+++ day2/sample.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, url_for, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+@app.route('/login_page')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/success/<name>')
+def success(name):
+   return render_template('welcome.html',name=name)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+@app.route('/student')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+@app.route('/test')
+def test_static():
+    return render_template('test.html')
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html',items = items)
